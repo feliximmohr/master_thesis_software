d17e6ecfb893ad078df758c97677880a63dfacd1
feliximmohr <felix.immohr@outlook.com>
2019-08-13 15:28:14 +0200

    change variable name to be saved to 'stimulus'

--- matlab_feature_extraction/generate_white_noise.m
+++ matlab_feature_extraction/generate_white_noise.m
@@ -4,5 +4,5 @@ fs = 44100;
 t = 1;
 s = rng(1234);
 wn = randn(fs*t, 1);
-wn = wn/max(abs(wn));
-save('../generated/wn.mat', 'wn')
+stimulus = wn/max(abs(wn));
+save('../generated/wn.mat', 'stimulus','fs')
