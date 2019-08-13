69a3935e03eb80f2e6b1382f3b2263bf8d55a729
feliximmohr <felix.immohr@outlook.com>
2019-08-13 18:53:46 +0200

    fix save() in parfor

--- matlab_feature_extraction/generate_earSignals.m
+++ matlab_feature_extraction/generate_earSignals.m
@@ -81,6 +81,6 @@ function save_variables(filename,var_names,variables)
 %SAVE_VARIABLES saves variables under their respective names in a .mat
 % file. This function prevents the MATLAB save function to fail an a parfor
 % loop.
-s = cell2struct(variables,var_names);
-save(filename, s)
+s = cell2struct(variables{1:length(var_names)},var_names,2);
+save([filename,'.mat'],'-struct','s')
 end
