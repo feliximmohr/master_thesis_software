9d8487ec00770b1b929ea0d3746fff83a7dfee5e
feliximmohr <felix.immohr@outlook.com>
2019-09-22 23:17:03 +0200

    added script to generate 'raw' tabular dataset structure with minimal redundandy

--- matlab_feature_extraction/gen_tab_2.m
+++ matlab_feature_extraction/gen_tab_2.m
@@ -10,13 +10,13 @@ dataset_dir = {
              '/media/feliximmohr/Storage/master_thesis/generated/exp1/'
              '/media/feliximmohr/Storage/master_thesis/generated/exp2/'};
 
-export_dir  =  '/media/feliximmohr/Storage/master_thesis/generated/database/raw/';
-           
+export_dir = '/media/feliximmohr/Storage/master_thesis/generated/database/raw/';
+
 % Define metadata
 % Listening positions
 x = [0.00; 0.50; 1.00; 0.00; 0.50; 1.00; 1.25;  0.00;  0.50;  1.00]*(-1);
 y = [0.75; 0.75; 0.75; 0.00; 0.00; 0.00; 0.00; -0.75; -0.75; -0.75];
-
+% SFS methods
 sfs_method = {
             'NFCHOA_M006'
             'NFCHOA_M013'
@@ -34,8 +34,9 @@ sfs_method = {
             'LWFS-VSS_r45'
             'WFS'                
                 };
+% Reproduction setup
 setup = {'circular_nls0056_dvs3.00'};
-
+% Number of subjects (listeners)
 n_subjects = 20;
 
 %% Initialization
@@ -56,18 +57,17 @@ for i=1:size(dataset_dir,1)
     % Create list of all filenames across datasets
     feature_filelist = [feature_filelist,feature_files];
     target_filelist = [target_filelist,target_files];
-    % Total number of files
+    % Total number of files (equals number of conditions*positions)
     n_files = n_files + num;
 end
 % Sort filelists
 feature_filelist = sort(feature_filelist);
 target_filelist = sort(target_filelist);
     
-% load metadata
-%par = load(fullfile(features_dir,features_files{1}),{'requests','data_size','cfHz'});
+% Load metadata
+par = load(feature_filelist{1},'requests','data_size','cfHz');
 par.requests = {'ild','itd','ic'};
 par.data_size = [32 100 360];
-par.cfHz = [80,114.49758,152.84849,195.48313,242.88001,295.57104,354.14758,419.26703,491.66023,572.13964,661.60848,761.07086,871.64303,994.56595,1131.2192,1283.1364,1452.0225,1639.7729,1848.4948,2080.5306,2338.4844,2625.2513,2944.0497,3298.4573,3692.4518 ,4130.4548,4617.3821,5158.6985,5760.4791,6429.4776,7173.2023,8000];
 
 % Initialize
 n_fb_channel = par.data_size(1);
@@ -77,9 +77,12 @@ n_frames_file = n_frames*n_angles;
 n_features = length(par.requests)*n_fb_channel;
 feature_data = zeros(n_frames_file*n_files, n_features);
 target_data = zeros(n_files*n_angles,n_subjects);
-global_id = (0:n_frames_file*n_files-1).';
-p_id = zeros(n_frames_file*n_files,1);
-c_id = zeros(n_frames_file*n_files,1);
+
+global_id = uint32(0:n_frames_file*n_files*n_subjects-1).';
+p_id = zeros(1, n_frames_file*n_files, 'uint8');
+c_id = zeros(1, n_frames_file*n_files, 'uint8');
+s_id = uint8(1:n_subjects).';
+s_id = repmat(s_id,n_frames_file*n_files,1);
 
 %% Processing
 
@@ -92,13 +95,13 @@ for j = 1:n_files
     % Reshape features to table structure
     [ild,itd,ic] = deal([]);
     if any(strcmp(par.requests,'ild'))
-        ild = reshape(features.ild, n_fb_channel,n_frames_file).';
+        ild = reshape(features.ild,n_fb_channel,n_frames_file).';
     end
     if any(strcmp(par.requests,'itd'))
-        itd = reshape(features.itd, n_fb_channel,n_frames_file).';
+        itd = reshape(features.itd,n_fb_channel,n_frames_file).';
     end
     if any(strcmp(par.requests,'ic'))
-        ic = reshape(features.ic, n_fb_channel,n_frames_file).';
+        ic = reshape(features.ic,n_fb_channel,n_frames_file).';
     end
     data = [ild itd ic];
     f_ind = (j-1)*n_frames_file+1:j*n_frames_file;
@@ -111,7 +114,7 @@ for j = 1:n_files
     % Create reference list
     [~,name,~] = fileparts(feature_filelist{j});
     name_metadata = strsplit(name,'_');
-    % ugly WFS workaround
+    % Ugly WFS workaround
     if length(name_metadata)<8
         cond_id = 14;
         c_id(f_ind) = cond_id;
@@ -128,14 +131,13 @@ for j = 1:n_files
     disp(['Reshaped file ' num2str(j)])
 end
 
+% Create lookup table for ID with python-style indexing 
+p_id = stretch_vector(p_id,n_subjects);
+c_id = stretch_vector(c_id,n_subjects);
+ref_list_ID_table = table(global_id,p_id.',c_id.',s_id,...
+    'VariableNames',{'global_id','pos_id','cond_id','subject_id'});
 
-% Create data tables for features and targets
-var_f = insert_num2str({'ILD_','ITD_','IC_'},par.cfHz,'Hz',0);
-var_t = strcat('subject_', strsplit(num2str(1:n_subjects)));
-feature_table = array2table(feature_data, 'VariableNames', var_f);
-target_table = array2table(target_data,'VariableNames', var_t);
-
-% create lookup table for position with python-style indexing
+% Create lookup table for position with python-style indexing
 pos_idx = (0:length(x)-1).';
 pos_table = table(pos_idx,x,y,'VariableNames',{'pos_id','x','y'});
 
@@ -143,23 +145,41 @@ pos_table = table(pos_idx,x,y,'VariableNames',{'pos_id','x','y'});
 cond_idx = (0:length(sfs_method)-1).';
 cond_table = table(cond_idx,sfs_method,'VariableNames',{'cond_id','sfs_method'});
 
-% creat lookup table for setup with python-style indexing
+% Creat lookup table for setup with python-style indexing
 setup_idx = (0:length(setup)-1).';
 setup_table = table(setup_idx,setup,'VariableNames',{'setup_id','setup'});
 
-% creat lookup table for ID with python-style indexing
-ref_list_ID_table = table(global_id,p_id,c_id,'VariableNames',{'global_id','pos_id','cond_id'});
+% Create data tables for features and targets
+var_t = strcat('subject_', strsplit(num2str(1:n_subjects)));
+target_table = array2table(target_data,'VariableNames', var_t);
+var_f = insert_num2str({'ILD_','ITD_','IC_'},par.cfHz,'Hz',0);
+feature_table = array2table(feature_data, 'VariableNames', var_f);
 
 %% Save to files
 
+% Write feature data
+disp('Writing feature data...');
+%writetable(feature_table,fullfile(export_dir,'feature_data.csv'));
+disp('Done.');
+
+% Write target data
+disp('Writing target data...');
+%writetable(target_table,fullfile(export_dir,'target_data.csv'));
+disp('Done.');
+
+% Write id reference table (global_id)
+disp('Writing id reference table...');
+writetable(ref_list_ID_table,fullfile(export_dir,'ID_reference_table.csv'));
+disp('Done.');
+
 % Write position table (pos_id)
 disp('Writing position table...');
-writetable(pos_table,fullfile(export_dir,'position_table.csv'));
+%writetable(pos_table,fullfile(export_dir,'position_table.csv'));
 disp('Done.');
 
 % Write condition table (cond_id)
 disp('Writing condition table...');
-writetable(cond_table,fullfile(export_dir,'condition_table.csv'));
+%writetable(cond_table,fullfile(export_dir,'condition_table.csv'));
 disp('Done.');
 
 % Write setup table (setup_id)
@@ -167,17 +187,20 @@ disp('Done.');
 %writetable(setup_table,'setup_table.csv');
 %disp('Done.');
 
-% Write id reference table (global_id)
-disp('Writing id reference table...');
-writetable(ref_list_ID_table,fullfile(export_dir,'ID_reference_table.csv'));
-disp('Done.');
+% =========================================================================
 
-% Write feature data
-disp('Writing feature data...');
-writetable(feature_table,fullfile(export_dir,'feature_data.csv'));
-disp('Done.');
+function [x_s] = stretch_vector(x,n)
+%STRETCH_VECTOR stretches a given row vector of length N by factor n, so
+% that the resulting length equals N*n.
+% For example: x = [1 2 3] n = 2 --> x_s = [1 1 2 2 3 3] 
+%
+%   Input parameters:
+%       x   - row vector [1xN]
+%       n   - integer factor
+%
+%   Output parameters:
+%       x_s - stretched row vector [1xN*n]
 
-% Write target data
-disp('Writing target data...');
-writetable(target_table,fullfile(export_dir,'target_data.csv'));
-disp('Everything done.');
+s = repmat(x,n,1);
+x_s = s(1:end);
+end
