b7d46971d7475e6f730cb32440ebb35713b51a19
feliximmohr <felix.immohr@outlook.com>
2019-09-28 17:37:02 +0200

    added train/test-split based on parameter substrings and changed notebook structure

--- python_model_DNN/load_data.py
+++ python_model_DNN/load_data.py
@@ -101,49 +101,87 @@ def get_metadata_from_filename(filename):
     
     return method, setup, position, dataset_name
 
-def get_ignore_ids(ignore_list, cond_lookup_df, pos_lookup_df=None, sub_lookup_df=None):
+def get_subset_ids(subset_list, cond_lookup_df, pos_lookup_df=None, subject_lookup_df=None):
     """
-    Get condition (cond_id) and position ids (pos_id) to ignore as specified in list of strings ignore_list.
-    Returns two unique lists for cond_id and pos_id, respectively.
+    Get condition (cond_id), position (pos_id) and subject ids of a subset of the data specified by subset_list.
+    Returns unique lists for cond_id, pos_id and subject_id, respectively.
     
     Parameters
     ----------
-    ignore_list : list
-        List of strings containing position, condition or subject parameter substrings to ignore, e.g. ['NFCHOA', 'pos1', 'subject_1']
+    subset_list : list
+        List of strings containing position, condition or subject parameter substrings to select the subset, e.g. ['NFCHOA', 'pos1', 'subject_1']
     cond_lookup_df : pandas.DataFrame object
         DataFrame object containing all conditions and corresponding indexes.
     pos_lookup_df : pandas.DataFrame object
         Not yet supported.
-    sub_lookup_df : pandas.DataFrame object
+    subject_lookup_df : pandas.DataFrame object
         Not yet supported.
         
     Returns
     -------
-    ignore_cond_ids : list
-        List of condition ids to ignore as specified by ignore_list.
-    ignore_pos_ids : list
-        List of position ids to ignore as specified by ignore_list.
-    ignore_sub_ids : list
-        List of subject ids to ignore as specified by ignore_list.
+    cond_ids_subset : list
+        List of condition ids of a subset as specified by subset_list.
+    pos_ids_subset : list
+        List of position ids of a subset as specified by subset_list.
+    subject_ids_subset : list
+        List of subject ids of a subset as specified by subset_list.
     """
-    ignore_cond_ids = []
-    ignore_pos_ids = []
-    ignore_sub_ids = []
-    cond_ign = []
-    pos_ign = []
-    sub_ign = []
-    # do for each element in ignore_list
-    for ign in ignore_list:
-        # check if element to ignore related to positions or conditions
-        if 'pos' in ign:
-            pos_ign = [int(ign[3:])-1]
-        elif 'subject' in ign:
-            sub_ign = [int(ign[8:])]
+    cond_ids_subset = []
+    pos_ids_subset = []
+    subject_ids_subset = []
+    cond_id = []
+    pos_id = []
+    subject_id = []
+    # do for each element in subset_list
+    for elem in subset_list:
+        # check if element related to positions, conditions or subjects
+        if 'pos' in elem:
+            pos_id = [int(elem[3:])-1]
+        elif 'subject' in elem:
+            subject_id = [int(elem[8:])]
         else:
-            cond_ign = cond_lookup_df.index[cond_lookup_df['sfs_method'].str.contains(ign)].tolist()
+            cond_id = cond_lookup_df.index[cond_lookup_df['sfs_method'].str.contains(elem)].tolist()
         # append to list
-        ignore_pos_ids.extend(pos_ign)
-        ignore_cond_ids.extend(cond_ign)
-        ignore_sub_ids.extend(sub_ign)
-    # return unique list
-    return list(set(ignore_cond_ids)), list(set(ignore_pos_ids)), list(set(ignore_sub_ids))
+        pos_ids_subset.extend(pos_id)
+        cond_ids_subset.extend(cond_id)
+        subject_ids_subset.extend(subject_id)
+    # return unique lists
+    return list(set(cond_ids_subset)), list(set(pos_ids_subset)), list(set(subject_ids_subset))
+
+def get_subset_sample_idx(ID_ref, cond_ids, pos_ids, subject_ids):
+    """ 
+    Get list of sample indices of a subset of the complete data set.
+    The subset is selected by condition, position and subject ids.
+    Returns a unique list of indices corresponding to exactly one samlpe in the complete data set.
+    
+    Parameters
+    ----------
+    ID_ref : pandas DataFrame object
+        .
+    cond_ids : list of int
+        .
+    pos_ids : list of int
+        .
+    subject_ids : list of int
+        .
+        
+    Returns
+    -------
+    idx_list : ndarray of uint32
+        .
+    """
+    # Initialization
+    idx_list = np.array([],dtype=np.uint32)
+    # irgendwas
+    for pos in pos_ids:
+        idx = ID_ref[(ID_ref.pos_id==pos)].index.values.astype(np.uint32)
+        idx_list = np.concatenate((idx_list, idx))
+    for cond in cond_ids:
+        idx = ID_ref[(ID_ref.cond_id==cond)].index.values.astype(np.uint32)
+        idx_list = np.concatenate((idx_list, idx))
+    for sub in subject_ids:
+        idx = ID_ref[(ID_ref.subject_id==sub)].index.values.astype(np.uint32)
+        idx_list = np.concatenate((idx_list, idx))
+
+    # Return unique idx list
+    return np.unique(idx_list)
