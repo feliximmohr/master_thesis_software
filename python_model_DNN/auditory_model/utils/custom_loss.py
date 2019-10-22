a624e528e46434e53afc56012e960f288f538f07
feliximmohr <felix.immohr@outlook.com>
2019-10-22 21:45:46 +0200

    added function to compute angle diff without tf

--- python_model_DNN/auditory_model/utils/custom_loss.py
+++ python_model_DNN/auditory_model/utils/custom_loss.py
@@ -4,14 +4,14 @@ A python module that provides different custom loss functions.
 
 import tensorflow as tf
 from keras import backend as K
-from numpy import pi
+from numpy import pi, sin, cos, rad2deg, deg2rad, arctan2
 
 def mse_wrap_angle(y_true, y_pred):
     """
     Custom loss function based on MSE but with angles wrapped to 360 degree.
     Tensorflows atan2 is used. Therefore conversion to radiant and back is performed.
     """
-    diff = angle_diff_deg(y_pred, y_true)
+    diff = tf_angle_diff_deg(y_pred, y_true)
     return K.mean(K.square(diff), axis=-1)
 
 def mae_wrap_angle(y_true, y_pred):
@@ -19,11 +19,11 @@ def mae_wrap_angle(y_true, y_pred):
     Custom loss function based on MAE but with angles wrapped to 360 degree.
     Tensorflows atan2 is used. Therefore conversion to radiant and back is performed.
     """
-    diff = angle_diff_deg(y_pred, y_true)
+    diff = tf_angle_diff_deg(y_pred, y_true)
     return K.mean(K.abs(diff), axis=-1)
 
-def angle_diff_deg(a,b):
-    """Calculates angle differences in degrees."""
+def tf_angle_diff_deg(a,b):
+    """Calculates angle differences in degrees in tensorflow."""
     a = tf_deg2rad(a)
     b = tf_deg2rad(b)
     diff = tf.atan2(tf.sin(a - b), tf.cos(a - b))
@@ -36,3 +36,10 @@ def tf_deg2rad(a):
 def tf_rad2deg(a):
     """Tensorflow compatible conversion from degree to radians."""
     return tf.divide(tf.multiply(a,180),pi)
+
+def angle_diff_deg(a,b):
+    """Calculates angle differences in degrees for evaluation outside tensorflow."""
+    a = deg2rad(a)
+    b = deg2rad(b)
+    diff = arctan2(sin(a - b), cos(a - b))
+    return rad2deg(diff)
